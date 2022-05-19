3cd26b9f99dc7ecce5ecb30d5bfe1a77ff8d4259
Danny <josedan.jd94@gmail.com>
2022-05-18 20:44:45 -0500

    first commit

---
+++ src/api/coordenada/routes/coordenada.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * coordenada router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::coordenada.coordenada');
---
+++ src/api/coordenada/services/coordenada.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * coordenada service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::coordenada.coordenada');
